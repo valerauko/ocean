93dadb53c1c9840b6cf3e0faaf384fd8dd31e6cf
Vale <valerauko@gmail.com>
2020-11-03 14:10:02 +0900

    Try using setup script

--- image/start.sh
+++ image/start.sh
@@ -1,7 +1,7 @@
 ./seafile.sh start
-# ./seahub.sh start
+./seahub.sh start
 
 while pgrep -f "seafile-controller"; do
-  echo "seafile-controller PID $(pgrep seafile-controller)"
+  echo "seafile-controller PID $(pgrep -f seafile-controller)"
   sleep 30;
 done
