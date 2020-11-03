a5bdee3e202b2378c9bdabf61b0272d2d1456e22
Vale <valerauko@gmail.com>
2020-11-03 17:06:44 +0900

    Bind seahub to 0.0.0.0

--- image/setup.sh
+++ image/setup.sh
@@ -5,6 +5,9 @@ rm /opt/seafile/ccnet /opt/seafile/conf /opt/seafile/seafile-data
 
 ./setup-seafile-mysql.sh "$@"
 
+# it shouldn't be binding to 127.0.0.1 (lo)
+sed -i 's/127.0.0.1/0.0.0.0/g' /opt/seafile/conf/gunicorn.conf.py
+
 for folder in ccnet conf seafile-data; do
   mkdir -pv /shared/seafile
   if [ -d "/opt/seafile/$folder" ]; then
