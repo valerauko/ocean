7b45ab00f1d3146cfa3f2ace5983f36f32264e94
Vale <valerauko@gmail.com>
2020-11-03 19:02:27 +0900

    Fix borked server url config

--- image/setup.sh
+++ image/setup.sh
@@ -7,6 +7,8 @@ rm /opt/seafile/ccnet /opt/seafile/conf /opt/seafile/seafile-data
 
 # it shouldn't be binding to 127.0.0.1 (lo)
 sed -i 's/127.0.0.1/0.0.0.0/g' /opt/seafile/conf/gunicorn.conf.py
+# fix broken hostname config
+sed -i "s/http:\/\/REPLACE_ME:8000/${SERVER_LOCATION}/g" /opt/seafile/conf/ccnet.conf
 
 for folder in ccnet conf seafile-data; do
   mkdir -pv /shared/seafile
