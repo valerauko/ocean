7758ebda69ab2d302e65bb3bd16442c93546feb5
Vale <valerauko@gmail.com>
2020-11-03 15:27:14 +0900

    Fix cannot create admin errors

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,6 +1,8 @@
+cp /shared/seafile/conf/admin.txt /shared/seafile/conf/admin.txt.dup
 ./seafile.sh start
+mv /shared/seafile/conf/admin.txt.dup /shared/seafile/conf/admin.txt
 ./seahub.sh start
 
-while pgrep -f "seafile-controller"; do
+while pgrep -f "seafile-controller" 1>/dev/null; do
   wait $(pgrep -f seafile-controller)
 done
